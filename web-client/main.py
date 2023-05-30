129b671370f4c5d477f698e32d5438b5451d3aa2
akitomonam <akitomonam@gmail.com>
2023-05-30 16:04:37 +0900

    python api test 実装

--- web-client/main.py
+++ web-client/main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello world"}
+@app.get("/helloworld")
+def hello_world():
+    return {"Hello": "World"}
+
+
+@app.get("/question")
+def return_answer(question_sentence: str):
+    return {"question_sentence": question_sentence}
